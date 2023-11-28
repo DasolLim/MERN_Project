d631349794a64e9ba5562aef27b195cf006135c2
Dasol Lim <dlim67@uwo.ca>
2023-11-27 23:40:02 -0500

    Implement frontend script to operate the website

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import Register from './pages/Register'
+
 function App() {
   return (
-    <div>
-      <h1>My App</h1>
-    </div>
-  );
+    <>
+      <Router>
+        <div className='container'>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Dashboard />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  )
 }
 
-export default App;
+export default App
