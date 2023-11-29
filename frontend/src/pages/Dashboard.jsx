9c49cd3b7a7e44ab9b290ca99d4e17b03d5d34e4
Dasol Lim <dlim67@uwo.ca>
2023-11-28 21:56:46 -0500

    Finalize Dashboard.jsx for display after use login

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,19 +2,38 @@ import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import GoalForm from '../components/GoalForm'
+import GoalItem from '../components/GoalItem'
+import Spinner from '../components/Spinner'
+import { getGoals, reset } from '../features/goals/goalSlice'
 
 function Dashboard() {
-
     const navigate = useNavigate()
+    const dispatch = useDispatch()
 
     const { user } = useSelector((state) => state.auth)
+    const { goals, isLoading, isError, message } = useSelector(
+        (state) => state.goals
+    )
 
     useEffect(() => {
+        if (isError) {
+            console.log(message)
+        }
 
         if (!user) {
             navigate('/login')
         }
-    }, [user, navigate])
+
+        dispatch(getGoals())
+
+        return () => {
+            dispatch(reset())
+        }
+    }, [user, navigate, isError, message, dispatch])
+
+    if (isLoading) {
+        return <Spinner />
+    }
 
     return (
         <>
@@ -24,6 +43,18 @@ function Dashboard() {
             </section>
 
             <GoalForm />
+
+            <section className='content'>
+                {goals.length > 0 ? (
+                    <div className='goals'>
+                        {goals.map((goal) => (
+                            <GoalItem key={goal._id} goal={goal} />
+                        ))}
+                    </div>
+                ) : (
+                    <h3>You have not set any goals</h3>
+                )}
+            </section>
         </>
     )
 }
