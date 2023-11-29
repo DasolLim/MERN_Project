c90540a7f761e1b2a4fcc51168ff45f0df54dca7
Dasol Lim <dlim67@uwo.ca>
2023-11-28 22:03:52 -0500

    Implement goal delete function

--- frontend/src/features/goals/goalSlice.js
+++ frontend/src/features/goals/goalSlice.js
@@ -47,6 +47,25 @@ export const getGoals = createAsyncThunk(
     }
 )
 
+// Delete user goal
+export const deleteGoal = createAsyncThunk(
+    'goals/delete',
+    async (id, thunkAPI) => {
+        try {
+            const token = thunkAPI.getState().auth.user.token
+            return await goalService.deleteGoal(id, token)
+        } catch (error) {
+            const message =
+                (error.response &&
+                    error.response.data &&
+                    error.response.data.message) ||
+                error.message ||
+                error.toString()
+            return thunkAPI.rejectWithValue(message)
+        }
+    }
+)
+
 export const goalSlice = createSlice({
     name: 'goal',
     initialState,
@@ -81,7 +100,22 @@ export const goalSlice = createSlice({
                 state.isError = true
                 state.message = action.payload
             })
-    }
+            .addCase(deleteGoal.pending, (state) => {
+                state.isLoading = true
+            })
+            .addCase(deleteGoal.fulfilled, (state, action) => {
+                state.isLoading = false
+                state.isSuccess = true
+                state.goals = state.goals.filter(
+                    (goal) => goal._id !== action.payload.id
+                )
+            })
+            .addCase(deleteGoal.rejected, (state, action) => {
+                state.isLoading = false
+                state.isError = true
+                state.message = action.payload
+            })
+    },
 })
 
 export const { reset } = goalSlice.actions
