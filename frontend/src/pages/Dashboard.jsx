45dcaa8fcdf2a52322ad294c20f37ce02704da97
Dasol Lim <dlim67@uwo.ca>
2023-11-28 20:37:20 -0500

    Update Dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
+import GoalForm from '../components/GoalForm'
 
 function Dashboard() {
 
@@ -16,7 +17,14 @@ function Dashboard() {
     }, [user, navigate])
 
     return (
-        <div>Dashboard</div>
+        <>
+            <section className='heading'>
+                <h1>Welcome {user && user.name}</h1>
+                <p>Goals Dashboard</p>
+            </section>
+
+            <GoalForm />
+        </>
     )
 }
 
