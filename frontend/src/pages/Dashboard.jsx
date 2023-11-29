2b0a82b7faddbb6e076e99e6126b87fd75212f22
Dasol Lim <dlim67@uwo.ca>
2023-11-28 20:17:09 -0500

    Implementing Login restriction to the website

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
 
 function Dashboard() {
+
+    const navigate = useNavigate()
+
+    const { user } = useSelector((state) => state.auth)
+
+    useEffect(() => {
+
+        if (!user) {
+            navigate('/login')
+        }
+    }, [user, navigate])
+
     return (
         <div>Dashboard</div>
     )
