d6cccb67ca88dafa97fb511299bbc2ebdc9a50f9
Dasol Lim <dlim67@uwo.ca>
2023-11-30 16:34:11 -0500

    Update frontend App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,33 @@
+// Used for handling routing in a React application
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+// Simple way to toast notification in a React app
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+// Components from specific file paths
 import Header from './components/Header'
 import Main from './pages/Main'
 import Dashboard from './pages/Dashboard'
 import Login from './pages/Login'
 import Register from './pages/Register'
 
+// App is the functional component in react
 function App() {
   return (
+    // <> is used to wrap multiple elements wihtout introducitng additonal DOM element
     <>
       <Router>
         <div className='container'>
           <Header />
+          {/* Define multiple routes in the application */}
           <Routes>
-            <Route path='main' element={<Main />} />
-            <Route path='/' element={<Dashboard />} />
+            <Route path='/' element={<Main />} />
+            <Route path='/dashboard' element={<Dashboard />} />
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
           </Routes>
         </div>
       </Router>
+      {/* Used to display toast notifications */}
       <ToastContainer />
     </>
   )
