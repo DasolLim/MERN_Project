c885582eb71451c8942fc82a1f041ad537170321
Dasol Lim <dlim67@uwo.ca>
2023-12-03 02:16:07 -0500

    Home page title, description, and links

--- frontend/src/components/GoalItem.jsx
+++ frontend/src/components/GoalItem.jsx
@@ -1,39 +1,82 @@
-import { useDispatch } from 'react-redux'
-import { deleteGoal } from '../features/goals/goalSlice'
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { deleteGoal } from '../features/goals/goalSlice';
 
 function GoalItem({ goal, onEdit }) {
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
     const [isEditing, setIsEditing] = useState(false);
+    const [showConfirmation, setShowConfirmation] = useState(false);
 
     const handleEditClick = () => {
         setIsEditing(true);
         onEdit(goal);
+    };
 
-        console.log('Editing goal with ID:', goal._id)
+    const handleCancelEdit = () => {
+        setIsEditing(false);
+    };
+
+    const handleDeleteClick = () => {
+        setShowConfirmation(true);
+    };
+
+    const handleConfirmDelete = () => {
+        dispatch(deleteGoal(goal._id));
+        setShowConfirmation(false);
+    };
+
+    const handleCancelDelete = () => {
+        setShowConfirmation(false);
+    };
+
+    const buttonContainerStyle = {
+        display: 'flex',
+        justifyContent: 'center',
+        gap: '10px',  // Adjust the gap as needed
     };
 
     return (
         <div className='goal'>
             <div>{new Date(goal.createdAt).toLocaleString('en-US')}</div>
-            <h2>{goal.text}</h2>
-            <p>Description: {goal.description}</p>
-            <button onClick={() => dispatch(deleteGoal(goal._id))} className='close'>
-                X
-            </button>
-            <button
-                onClick={handleEditClick}
-                style={{
-                    display: 'block',
-                    margin: '15px auto 0',
-                }}
-                className='btn'
-            >
-                Edit
-            </button>
+            {isEditing ? (
+                <div>
+                    <button onClick={handleCancelEdit}>Cancel</button>
+                </div>
+            ) : (
+                <>
+                    <h2>{goal.text}</h2>
+                    <p>Description: {goal.description}</p>
+                    <button onClick={handleDeleteClick} className='close'>
+                        X
+                    </button>
+                    <button
+                        onClick={handleEditClick}
+                        style={{
+                            display: 'block',
+                            margin: '10px auto 10px',
+                        }}
+                        className='btn'
+                    >
+                        Edit
+                    </button>
+                </>
+            )}
 
+            {showConfirmation && (
+                <div>
+                    <p>Confirmation: Delete Goal?</p>
+                    <div style={buttonContainerStyle}>
+                        <button onClick={handleConfirmDelete} className='btn'>
+                            Yes
+                        </button>
+                        <button onClick={handleCancelDelete} className='btn'>
+                            No
+                        </button>
+                    </div>
+                </div>
+            )}
         </div>
-    )
+    );
 }
 
-export default GoalItem
+export default GoalItem;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,17 +4,43 @@ const Home = () => {
     return (
         <div className="home">
             <header>
-                <h1>Welcome to Your Website</h1>
+                <h1>WELCOME TO SE3316 LAB 4</h1>
                 <p>Explore, Discover, Enjoy</p>
             </header>
             <main>
                 <section>
-                    <h2>Featured Content</h2>
-                    {/* Add your featured content components or information here */}
+                    <h2>Description</h2>
+                    <p>
+                        The website is an advanced platform designed for the comprehensive management of heroes,
+                        featuring a robust authentication system with Firebase and JWT for secure user interactions.
+                        It offers a range of user-centric functionalities, including search capabilities, list creation,
+                        and review submissions. Additionally, administrators have access to tools for efficient user
+                        management and overall site maintenance. The website places a strong emphasis on security, privacy,
+                        and adherence to DMCA guidelines, ensuring a professional and compliant online experience.
+                    </p>
                 </section>
                 <section>
-                    <h2>Latest Updates</h2>
-                    {/* Add your latest updates components or information here */}
+                    <h2>Featured Content</h2>
+                    <ul>
+                        <li>
+                            Authentication
+                        </li>
+                        <li>
+                            User Functionality
+                        </li>
+                        <li>
+                            Administrator Features
+                        </li>
+                        <li>
+                            Web Service API
+                        </li>
+                        <li>
+                            Copyright Enforcement
+                        </li>
+                        <li>
+                            DMCA Takedown Procedure
+                        </li>
+                    </ul>
                 </section>
             </main>
             <footer>
