083e6714a36f55ae3199d1f9fe6d724c6d4ef150
Dasol Lim <dlim67@uwo.ca>
2023-12-04 16:22:36 -0500

    Ability to mark a user as “deactivated”

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,6 +4,7 @@ const {
     registerUser,
     loginUser,
     getMe,
+    grantAdminPrivilege,
 } = require('../controllers/userController')
 
 const { protect } = require('../middleware/authMiddleware')
@@ -11,5 +12,10 @@ const { protect } = require('../middleware/authMiddleware')
 router.post('/', registerUser)
 router.post('/login', loginUser)
 router.get('/me', protect, getMe)
+
+// testing: /api/users/:id/grant-admin (grant admin privilege to other users)
+router.put('/:id/grant-admin', protect, grantAdminPrivilege);
+// testing /api/users/:id/update-deactivation (admin can mark any user account as "deactivated")
+router.put('/:id/update-deactivation', protect, updateDeactivationStatus);
 
 module.exports = router
