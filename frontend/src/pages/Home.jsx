d2731a3c89cb2ea3152c6833c9142e5026f6a353
Dasol Lim <dlim67@uwo.ca>
2024-10-14 05:04:17 -0400

    Ready for deployment

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-// import myImage from 'MERN-Project/frontend/src/pages/homePagePicture.png'; // Import the image
+import myImage from './homePagePicture.png';
 
 const Home = () => {
     return (
         <div className="home">
             <header>
                 <h1>MERN Stack Project</h1>
+                <div className="image-container">
+                    <img src={myImage} alt="Description of the image" />
+                </div>
                 <p>MongoDB, Express. js, React, and Node. js</p>
             </header>
             <main>
                 <section>
-                    <div>
-                        {/* <img src={myImage} alt="Description of the image" /> */}
-                    </div>
-                    <h2>Description</h2>
+                    <h2 id="home-header">Project Description</h2>
                     <p>
                         The website is an advanced platform designed for the comprehensive management of heroes,
                         featuring a robust authentication system with Firebase and JWT for secure user interactions.
@@ -24,31 +24,43 @@ const Home = () => {
                     </p>
                 </section>
                 <section>
-                    <h2>Featured Content</h2>
-                    <ul>
-                        <li>
-                            Authentication
-                        </li>
-                        <li>
-                            User Functionality
-                        </li>
-                        <li>
-                            Administrator Features
-                        </li>
-                        <li>
-                            Web Service API
-                        </li>
-                        <li>
-                            Copyright Enforcement
-                        </li>
-                        <li>
-                            DMCA Takedown Procedure
-                        </li>
-                    </ul>
+                    <h2 id="home-header">Featured Content</h2>
+                    <div class="featured-container">
+                        <h3>Authentication:</h3>
+                        <p>
+                            In our web app, creating an account is as easy as pie. You just need to toss in your email, a password, and a nickname. Plus, we've got your back with email validation, making sure everything's in the right format.
+                        </p>
+
+                        <h3>User Functionality:</h3>
+                        <p>
+                            For starters, our homepage is like the gateway to awesome. It showcases the app's name, throws in a quick rundown, and sets you up for a smooth login.
+                        </p>
+                        <p>
+                            Unleash your inner hero searcher! You can hunt down your favorite heroes based on their name, race, power, or publisher. And, no worries if you have a typo; our search terms are pretty chill.
+                        </p>
+                        <p>
+                            Ever wanted to keep a list of your top heroes? Authenticated users can create up to 20 named hero lists. Oh, and if you're feeling generous, you can share public lists and let others add reviews.
+                        </p>
+
+                        <h3>Administrator Features:</h3>
+                        <p>
+                            We've got a superhero admin, making sure everything's in check. They can grant special privileges, manage reviews (hiding or revealing them), and even handle user accounts.
+                        </p>
+
+                        <h3>Web Service API:</h3>
+                        <p>
+                            Behind the scenes, we've tweaked our API to make sure it plays nice with all the cool stuff you're doing. It's the secret sauce connecting your front-end to the powerful back-end.
+                        </p>
+
+                        <h3>Copyright Enforcement and DMCA Takedown Procedure:</h3>
+                        <p>
+                            Now, let's talk about keeping it all legit. We've laid down clear policies for security, privacy, and handling copyright issues. There's even a nifty DMCA takedown procedure and tools to manage the nitty-gritty details.
+                        </p>
+                    </div>
                 </section>
             </main>
-            <footer>
-                <p>&copy; 2023 Your Website. All rights reserved.</p>
+            <footer class="footer-container">
+                <p><i>&copy; 2023 Your Website. All rights reserved.</i></p>
             </footer>
         </div>
     );
